35e8444df2d8db7d71e1f4ade7201edb26b7bad1
diasoy <22082010134@student.upnjatim.ac.id>
2024-04-09 01:39:34 +0700

    appBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
-import 'package:flutter_study/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +12,42 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoginPage(),
-    );
+        home: Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.adb, color: Colors.white),
+        title: Text(
+          'AppBar Example',
+          style: TextStyle(color: Colors.white),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () {
+              print('Settings Button is clicked');
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              print('Search Button is clicked');
+            },
+          ),
+        ],
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.blue, Colors.blueAccent],
+              begin: FractionalOffset.topLeft,
+              end: FractionalOffset.bottomRight,
+            ),
+            image: DecorationImage(
+              image: AssetImage('assets/images/logoflutter.png'),
+              fit: BoxFit.none,
+              repeat: ImageRepeat.repeat,
+            ),
+          ),
+        ),
+      ),
+    ));
   }
 }
