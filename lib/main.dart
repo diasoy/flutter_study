c4567c23a6ac5ae9254182b1e41f1cee39021a11
diasoy <22082010134@student.upnjatim.ac.id>
2024-04-16 18:49:41 +0700

    textfield widget part2

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,38 @@ class _MyAppState extends State<MyApp> {
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: <Widget>[
                 TextField(
+                  decoration: InputDecoration(
+                    fillColor: Colors.indigo[100],
+                    filled: true,
+                    // prefix: Container(
+                    //   padding: EdgeInsets.all(10),
+                    //   child: Text('Prefix'),
+                    // ),
+                    suffix: Container(
+                      width: 5,
+                      height: 5,
+                      color: Colors.red,
+                    ),
+                    hintText: 'Enter your password',
+                    hintStyle: TextStyle(
+                      color: Colors.red,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    labelText: 'Password',
+                    labelStyle: TextStyle(
+                      color: Colors.green,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    icon: Icon(Icons.lock),
+                    prefixIcon: Icon(Icons.lock),
+                    prefixText: "Password: ",
+                    prefixStyle: TextStyle(
+                        color: Colors.blue, fontWeight: FontWeight.bold),
+                    suffixIcon: Icon(Icons.remove_red_eye),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
                   maxLength: 10,
                   obscureText: true,
                   onChanged: (value) {
