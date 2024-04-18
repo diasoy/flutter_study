a5cb60cf579527acf1f72a60b2a1a5602edcd1eb
diasoy <22082010134@student.upnjatim.ac.id>
2024-04-18 22:53:00 +0700

    audio player

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,83 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, must_be_immutable, prefer_const_constructors_in_immutables, avoid_unnecessary_containers, sort_child_properties_last
 
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MainPage();
-  }
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MainPage extends StatelessWidget {
-  const MainPage({super.key});
+class _MyAppState extends State<MyApp> {
+  late AudioPlayer audioPlayer;
+  String durasi = "00:00:00";
+
+  _MyAppState() {
+    audioPlayer = AudioPlayer();
+    audioPlayer.onAudioPositionChanged.listen((Duration duration) {
+      setState(() {
+        durasi = duration.toString().split('.').first;
+      });
+    });
+    audioPlayer.setReleaseMode(ReleaseMode.LOOP);
+  }
+
+  void playSound() async {
+    await audioPlayer.play('https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3');
+  }
+
+  void pauseSound() async {
+    await audioPlayer.pause();
+  }
+
+  void resumeSound() async {
+    await audioPlayer.resume();
+  }
+
+  void stopSound() async {
+    await audioPlayer.stop();
+  }
 
   @override
   Widget build(BuildContext context) {
-    TabBar myTabBar = TabBar(
-      indicatorColor: Colors.red,
-      indicator: BoxDecoration(
-        color: Colors.blue,
-      ),
-      tabs: [
-        Tab(
-          icon: Icon(Icons.comment),
-          text: "Comments",
-        ),
-        Tab(
-          icon: Icon(Icons.computer),
-          text: "Computer",
-        ),
-      ],
-    );
-
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text('TabBar'),
-          ),
-          bottomNavigationBar: PreferredSize(
-            preferredSize: Size.fromHeight(myTabBar.preferredSize.height),
-            child: Container(
-              color: Colors.grey,
-              child: myTabBar,
-            ),
-          ),
-          body: TabBarView(
-            children: [
-              Center(
-                child: Text('Comments'),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Audio Player')),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () {
+                  playSound();
+                },
+                child: Text('Play'),
               ),
-              Center(
-                child: Text('Computer'),
+              ElevatedButton(
+                onPressed: () {
+                  pauseSound();
+                },
+                child: Text('Pause'),
               ),
+              ElevatedButton(
+                onPressed: () {
+                  resumeSound();
+                },
+                child: Text('Resume'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  stopSound();
+                },
+                child: Text('Stop'),
+              ),
+              Text(durasi, style: TextStyle(fontSize: 20)),
             ],
           ),
         ),
