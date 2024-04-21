bc1aafb6895214bfef8591428f443b68ff7068b5
diasoy <22082010134@student.upnjatim.ac.id>
2024-04-21 14:08:34 +0700

    provider state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,85 +1,61 @@
 // ignore_for_file: prefer_const_constructors_in_immutables
 
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_study/application_color.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  TextEditingController controller = TextEditingController(text: 'No Name');
-  bool isOn = false;
-
-  void saveData() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setString('name', controller.text);
-    prefs.setBool('isOn', isOn);
-  }
-
-  void loadData() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    setState(() {
-      controller.text = prefs.getString('name') ?? 'No Name';
-      isOn = prefs.getBool('isOn') ?? false;
-    });
-  }
-
-  Future<String> getName() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    return prefs.getString('name') ?? 'No Name';
-  }
-
-  Future<bool> getIsOn() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    return prefs.getBool('isOn') ?? false;
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Shared Preferences'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              TextField(
-                controller: controller,
-              ),
-              Switch(
-                value: isOn,
-                onChanged: (value) {
-                  setState(() {
-                    isOn = value;
-                  });
-                },
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  saveData();
-                },
-                child: const Text('Save'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  loadData();
-                },
-                child: const Text('Load'),
-              )
-            ],
+        home: ChangeNotifierProvider<ApplicationColor>(
+      create: (context) => ApplicationColor(),
+      child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.black,
+            title: Consumer<ApplicationColor>(
+                builder: (context, applicationColor, _) => Text(
+                      'Provider State Management',
+                      style: TextStyle(color: applicationColor.color),
+                    )),
           ),
-        ),
-      ),
-    );
+          body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Consumer<ApplicationColor>(
+                    builder: (context, applicationColor, _) =>
+                        AnimatedContainer(
+                      margin: EdgeInsets.all(10),
+                      height: 100,
+                      width: 100,
+                      color: applicationColor.color,
+                      duration: Duration(seconds: 1),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(child: Text('AB')),
+                      Consumer<ApplicationColor>(
+                        builder: (context, applicationColor, _) => Switch(
+                          value: applicationColor.isLightBlue,
+                          onChanged: (value) {
+                            applicationColor.isLightBlue = value;
+                          },
+                        ),
+                      ),
+                      Container(child: Text('CD')),
+                    ],
+                  )
+                ]),
+          )),
+    ));
   }
 }
